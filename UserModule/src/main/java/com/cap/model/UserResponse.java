327f9e432adc4208907b7045bd97927e284f4b84
Ramkesva <saikumar14551821>
2021-12-15 20:57:33 +0530

    Added User Module

---
+++ UserModule/src/main/java/com/cap/model/UserResponse.java
@@ -0,0 +1,32 @@
+package com.cap.model;
+
+
+public class UserResponse {
+	
+private String token;
+
+public UserResponse() {
+	super();
+	// TODO Auto-generated constructor stub
+}
+
+public String getToken() {
+	return token;
+}
+
+public void setToken(String token) {
+	this.token = token;
+}
+
+public UserResponse(String token) {
+	super();
+	this.token = token;
+}
+
+@Override
+public String toString() {
+	return "UserResponse [token=" + token + "]";
+}
+
+
+}
